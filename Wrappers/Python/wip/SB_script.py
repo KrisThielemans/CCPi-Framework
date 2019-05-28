bec5386afac7f40f91728666ed9286b12ba498f5
Evelina Ametova <EvelinaAmetova@gmail.com>
2019-05-28 09:02:05 +0100

    Split Bregman: script implementation using data containers

--- Wrappers/Python/wip/SB_script.py
+++ Wrappers/Python/wip/SB_script.py
@@ -6,7 +6,7 @@ Created on Mon May 27 11:55:04 2019
 @author: evelina
 """
 
-from ccpi.framework import ImageData, ImageGeometry, AcquisitionGeometry, AcquisitionData
+from ccpi.framework import ImageData, ImageGeometry, AcquisitionGeometry, AcquisitionData, DataContainer
 
 import numpy as np 
 import matplotlib.pyplot as plt
@@ -21,11 +21,16 @@ from ccpi.optimisation.operators import FiniteDiff, SparseFiniteDiff
 
 # create phantom
 N = 75
-x = np.zeros((N,N))
-x[round(N/4):round(3*N/4),round(N/4):round(3*N/4)] = 0.5
-x[round(N/8):round(7*N/8),round(3*N/8):round(5*N/8)] = 1
+phantom = np.zeros((N,N))
+phantom[round(N/4):round(3*N/4),round(N/4):round(3*N/4)] = 0.5
+phantom[round(N/8):round(7*N/8),round(3*N/8):round(5*N/8)] = 1
 
-data = ImageData(x)
+# show Ground Truth
+plt.imshow(phantom)
+plt.title('Ground Truth')
+plt.show()
+
+data = ImageData(phantom)
 ig = ImageGeometry(voxel_num_x = N, voxel_num_y = N)
 
 detectors = N
@@ -36,26 +41,25 @@ Aop = AstraProjectorSimple(ig, ag, 'cpu')
 
 # generate sinogram
 b = Aop.direct(data)
-
+# show Sinogram
 plt.imshow(b.as_array())
-plt.title('sinogram')
+plt.title('Sinogram')
 plt.show()
 
+# scale coefficient - used to scale Aop.adjoint(Aop.direct(x))
 scale = 1 / np.sqrt(N * N)
 
 # init
 x = Aop.adjoint(b) * scale
-
+# show Initialization image
 plt.imshow(x.as_array())
-plt.title('init')
+plt.title('Initialization image')
 plt.show()
 
-d_0 = np.zeros((N, N), dtype = np.float32)
 # initialize some variables
 sx = ImageData(array = np.zeros((N, N), dtype = np.float32))
 sy = ImageData(array = np.zeros((N, N), dtype = np.float32))
-zero_matrix = np.zeros((N, N), dtype = np.float32)
-div_tol = 1e-12 * np.ones((N, N), dtype = np.float32)
+div_tol = 1e-12
 
 # constants
 # number of iterations
@@ -65,7 +69,7 @@ sb_mu = 1
 # Split Bregman lambda
 sb_lambda = 1
 # Split Bregman tolerance
-sb_tol = 1e-5 * np.sqrt(np.sum(np.sum(np.square(x), axis = 0), axis = 0))
+sb_tol = 1e-5 * x.norm()
 # Gradient Descent alpha
 gd_alpha = 1e-3
 # Gradient Descent number of iterations
@@ -75,8 +79,9 @@ gd_iter = 30
 FDx = FiniteDiff(ig, direction = 0, bnd_cond = 'Neumann')
 FDy = FiniteDiff(ig, direction = 1, bnd_cond = 'Neumann')
 
+# initialize other variables
 k = 0
-err = np.sum(np.sum(np.square(x), axis = 0), axis = 0)
+err = x.squared_norm()
 
 while ((err > sb_tol) and (k < sb_iter)):
     
@@ -85,28 +90,50 @@ while ((err > sb_tol) and (k < sb_iter)):
     # update d 
     gx = FDx.direct(x)
     gy = FDy.direct(x)
-    h = np.sqrt(np.square(gx.as_array() + sx.as_array()) + np.square(gy.as_array() + sy.as_array()))
-    dx = ImageData(array = np.maximum(h - 1 / sb_mu, zero_matrix) * (gx.as_array() + sx.as_array()) / np.maximum(div_tol, h))
-    dy = ImageData(array = np.maximum(h - 1 / sb_mu, zero_matrix) * (gy.as_array() + sy.as_array()) / np.maximum(div_tol, h))
+    h = ((gx + sx) ** 2 + (gy + sy) ** 2).sqrt()
+    dx = (h - 1 / sb_mu).maximum(0) * (gx + sx) / h.maximum(div_tol)
+    dy = (h - 1 / sb_mu).maximum(0) * (gy + sy) / h.maximum(div_tol)
     
     # update x using Gradient Descent
     for i in range(gd_iter):
-        x -= gd_alpha * (sb_lambda * scale * Aop.adjoint(Aop.direct(x) - b) + sb_mu * (FDx.adjoint(FDx.direct(x) - dx + sx) + FDy.adjoint(FDy.direct(x) - dy + sy)))
+        x -= gd_alpha * (sb_lambda * scale * Aop.adjoint(Aop.direct(x) - b) + 
+                         sb_mu * (FDx.adjoint(FDx.direct(x) - dx + sx) + FDy.adjoint(FDy.direct(x) - dy + sy)))
     
-    err = np.sum(np.sum(np.square(x.as_array() - x_0.as_array()), axis =0), axis =0)
+    err = (x - x_0).squared_norm()
     k += 1
     
     print('iter {}, err {}'.format(k, err))
     
     # update s
-    sx = sx + FDx.direct(x) - dx
-    sy = sy + FDy.direct(x) - dy
+    sx += FDx.direct(x) - dx
+    sy += FDy.direct(x) - dy
     '''
     plt.imshow(x.as_array())
     plt.title('x iter {}'.format(k))
     plt.show()
     '''
 
+# Show Ground Truth and Reconstruction
+plt.figure(figsize=(10,10))
+plt.subplot(2,1,1)
+plt.imshow(data.as_array())
+plt.title('Ground Truth')
+plt.colorbar()
+plt.subplot(2,1,2)
 plt.imshow(x.as_array())
-plt.title('res')
+plt.title('Reconstruction')
+plt.colorbar()
+plt.show()
+
+plt.figure(figsize=(10,10))
+plt.subplot(2,1,1)
+plt.plot(np.linspace(0,N,N), data.as_array()[int(N/2),:], label = 'Ground Truth')
+plt.plot(np.linspace(0,N,N), x.as_array()[int(N/2),:], label = 'Reconstruction')
+plt.legend()
+plt.title('Horizontal Line Profiles')
+plt.subplot(2,1,2)
+plt.plot(np.linspace(0,N,N), data.as_array()[:, int(N/2)], label = 'Ground Truth')
+plt.plot(np.linspace(0,N,N), x.as_array()[:, int(N/2)], label = 'Reconstruction')
+plt.legend()
+plt.title('Verical Line Profiles')
 plt.show()
